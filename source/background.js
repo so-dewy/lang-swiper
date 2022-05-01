ffa52aa87b0e24d2fc4aaceb7fcf934e6764c468
Roman Azami <nascentsoul13@gmail.com>
2022-05-01 23:26:19 +0300

    Add access to selected text and add a sample popup

--- source/background.js
+++ source/background.js
@@ -1,2 +1,13 @@
 // eslint-disable-next-line import/no-unassigned-import
 import './options-storage.js';
+import browser from 'webextension-polyfill';
+
+browser.contextMenus.create({
+  id: `cpy-as-md:selection`,
+  title: `Grab new vocabulary`,
+  contexts: ['selection']
+});
+
+browser.contextMenus.onClicked.addListener((info, tab) => {
+  console.log(info.selectionText);
+});
