4b8dd868bc39713e9db5463718145dd1051379fe
Roman Azami <nascentsoul13@gmail.com>
2022-05-02 22:43:22 +0300

    Add word segmentation to selected text

--- source/background.js
+++ source/background.js
@@ -1,6 +1,9 @@
 // eslint-disable-next-line import/no-unassigned-import
 import './options-storage.js';
 import browser from 'webextension-polyfill';
+import { Segment, useDefault } from 'segmentit';
+ 
+const segmentit = useDefault(new Segment());
 
 browser.contextMenus.create({
   id: `cpy-as-md:selection`,
@@ -10,4 +13,9 @@ browser.contextMenus.create({
 
 browser.contextMenus.onClicked.addListener((info, tab) => {
   console.log(info.selectionText);
+
+  const result = segmentit.doSegment(info.selectionText, {
+    simple: true
+  });
+  console.log(result);
 });
